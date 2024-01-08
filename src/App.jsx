013eba5a46fe56c53602a0e978f1658805c8051d
Klaudija Kolaric <klaudija.cenkovcan@gmail.com>
2024-01-08 13:47:31 +0100

    add todolist and render todoitem

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Title, VisibilityToolbar, AddTodoForm, TodoList } from './components';
+import styles from './App.module.css';
 
 export default function App() {
   return (
     <div>
       <Title text='My tasks' />
       <VisibilityToolbar />
-      <AddTodoForm />
-      <TodoList />
+      <div className={styles['form-and-list-wrapper']}>
+        <AddTodoForm />
+        <TodoList />
+      </div>
     </div>
   );
 }
